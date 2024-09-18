a9a5207b129cf22a56fe03a5707aaefc33955afa
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-09-18 17:06:03 -0300

    refact: schema.sql

--- init/schema.sql
+++ init/schema.sql
@@ -1,24 +1,26 @@
 CREATE DATABASE IF NOT EXISTS study_database;
 
+USE study_database;
+
 CREATE TABLE students (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     enrollment_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE courses (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     description TEXT,
     start_date DATETIME NOT NULL
 );
 
 CREATE TABLE student_courses (
-    student_id INTEGER,
-    course_id INTEGER,
+    student_id INT,
+    course_id INT,
     enrollment_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (student_id) REFERENCES students (id),
-    FOREIGN KEY (course_id) REFERENCES courses (id),
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (course_id) REFERENCES courses(id),
     PRIMARY KEY (student_id, course_id)
 );
