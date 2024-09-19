7ddfc000069f6d7c87ec30423fc14069a2285044
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-09-19 17:18:18 -0300

    feat: add select_user to StudentsRepository

--- src/infra/db/repositories/students_repository.py
+++ src/infra/db/repositories/students_repository.py
@@ -1,5 +1,6 @@
 from src.infra.db.settings.connection import DBConnectionHandler
 from src.infra.db.entities.students import Students as StudentsEntity
+from sqlalchemy import or_
 
 class StudentsRepository:
     
@@ -10,6 +11,16 @@ class StudentsRepository:
                 new_registry = StudentsEntity(name=name, email=email)
                 database.db_session.add(new_registry)
                 database.db_session.commit()
+            except Exception as exception:
+                database.db_session.rollback()
+                raise exception
+    
+    @classmethod
+    def select_student(cls, search: str = '') -> any:
+        with DBConnectionHandler() as database:
+            try:
+                students_on_db = database.db_session.query(StudentsEntity).filter(or_(StudentsEntity.name.ilike(f'%{search}%'),StudentsEntity.email.ilike(f'%{search}%'))).all()
+                return students_on_db
             except Exception as exception:
                 database.db_session.rollback()
                 raise exception
