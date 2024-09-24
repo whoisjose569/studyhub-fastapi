29bb5db9dbc622cbaecbe4dc2bd4498357d10ce0
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-09-24 09:29:01 -0300

    feat: implementing student_finder use_cases

--- src/data/use_cases/student_finder.py
+++ src/data/use_cases/student_finder.py
@@ -1,9 +1,26 @@
 from src.domain.use_cases.students_finder import StudentFinder as StudentFinderInterface
 from src.data.interfaces.student_repository import StudentsRepositoryInterface
 
+
 class StudentFinder(StudentFinderInterface):
     def __init__(self, students_repository: StudentsRepositoryInterface) -> None:
         self.__students_repository = students_repository
     
     def find(self, search: str) -> dict:
-        pass
+        students = self.__students_repository.select_student(search)
+        if students == []:
+            raise Exception("No students found with the given search term")
+        
+        response = {
+            "type": "Students",
+            "count": len(students),
+            "data": [{
+                "id": student.id,
+                "name": student.name,
+                "email": student.email,
+                "enrollment_date": student.enrollment_date
+            }
+            for student in students
+            ]
+        }
+        return response
