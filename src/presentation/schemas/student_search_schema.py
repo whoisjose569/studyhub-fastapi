791075baf5650f68ed2988831f4f7e5207e6c539
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-10-08 16:38:34 -0300

    feat: implementing student schemas and validators

---
+++ src/presentation/schemas/student_search_schema.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field, constr, validator, EmailStr
+from typing import Optional
+
+class StudentSearchSchema(BaseModel):
+    search: Optional[constr(max_length=100)] = Field(default="") # type: ignore
+
+    @validator('search')
+    def check_search(cls, v):
+        if v and not v.isalnum() and not all(c in ' @._-' for c in v):
+            raise ValueError('search must contain only alphanumeric characters, spaces, and @._-')
+        return v
+
+class StudentInputSchema(BaseModel):
+    name: str = Field(..., min_length=1, max_length=100)
+    email: EmailStr  
+
+    @validator('name')
+    def name_must_contain_only_letters_and_spaces(cls, v):
+        if not all(c.isalpha() or c.isspace() for c in v):
+            raise ValueError('Name must contain only letters and spaces.')
+        return v
+
+    class Config:
+        anystr_strip_whitespace = True
