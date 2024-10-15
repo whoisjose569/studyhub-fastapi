1dea8abf8da2ac1175101333efd7dfb48b38aa7d
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-10-15 15:55:44 -0300

    feat: implementing student_finder_by_id_controller

---
+++ src/presentation/controllers/student_finder_by_id_controller.py
@@ -0,0 +1,18 @@
+from src.presentation.http_types.http_request import HttpRequest
+from src.presentation.http_types.http_response import HttpResponse
+from src.presentation.interfaces.controller_interface import ControllerInterface
+from src.domain.use_cases.student_finder_by_id import StudentFinderById as StudentFinderByIdInterface
+
+class StudentFinderByIdController(ControllerInterface):
+    def __init__(self, use_case: StudentFinderByIdInterface) -> None:
+        self.__use_case = use_case
+    
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        id = http_request.path_params['id']
+        
+        response = self.__use_case.find_by_id(id)
+        
+        return HttpResponse(
+            status_code=200,
+            body={"data": response}
+        )
