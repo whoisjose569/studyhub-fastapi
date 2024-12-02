0056726889c0a50cec6402e44b63180c93d9a5ed
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-12-02 08:28:00 -0300

    feat: implementing student_update_composer on main layer

---
+++ src/main/composers/student_update_composer.py
@@ -0,0 +1,10 @@
+from src.infra.db.repositories.students_repository import StudentsRepository
+from src.data.use_cases.student_update import StudentUpdater
+from src.presentation.controllers.student_update_controller import StudentUpdateController
+
+def student_updater_composer():
+    repository = StudentsRepository()
+    use_case = StudentUpdater(repository)
+    controller = StudentUpdateController(use_case)
+    
+    return controller.handle
